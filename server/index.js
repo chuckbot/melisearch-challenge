937aae71d939caec2c9a3fb9260621d489e9d195
Carlos Garcia <cgmilan@gmail.com>
2020-12-07 22:21:19 -0400

    MeliSearch Backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
-'use strict'
-
 var app = require('./app');
 var port = 3001;
 
 app.listen(port, () => {
-    console.log('Server running in port ' + port)
+    console.log('Server running in port ' + port);
 });
